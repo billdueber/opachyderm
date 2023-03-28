9bf0d8b2343b42f164a9862cd0452c27beb1d8b7
Bill Dueber <bill@dueber.com>
2023-03-28 14:36:23 -0400

    search view kind of working

--- opachyderm_app.rb
+++ opachyderm_app.rb
@@ -2,6 +2,29 @@
 
 require "sinatra/base"
 require "pathname"
+require "json"
+
+ROOT = Pathname.new(__dir__).realdirpath
+DATA = ROOT + "spec" + "data"
+FAKEDATA = JSON.parse(File.read(DATA + "alzheimers.json"))
+
+# Screwed up and forgot facet = arrarr
+
+fd_facets = FAKEDATA["facet_counts"]["facet_fields"]
+fd_facets.each_pair do |category, values|
+  arrarr = []
+  while values.any?
+    term = values.shift
+    count = values.shift
+    arrarr.push [term, count]
+  end
+  fd_facets[category] = arrarr.take(10)
+end
+
+FAKEDATA["facet_counts"]["facet_fields"] = fd_facets
+
+
+
 
 Tilt.register Tilt[:erb], :"html.erb"
 
@@ -17,11 +40,17 @@ module Opachyderm
     end
 
     get "/search/:search" do |search|
-
-      erb "The terms are #{search}", layout: :layout
+      erb "", layout: :layout, 
+      locals: {
+        search_terms: search, 
+        documents: FAKEDATA["response"]["docs"],
+        facets: FAKEDATA["facet_counts"]["facet_fields"],
+        fake_data: FAKEDATA
+      }
     end
 
 
 
   end
 end
+
